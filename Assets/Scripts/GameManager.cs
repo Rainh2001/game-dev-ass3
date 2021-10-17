a7efc9e8637f0db1d32430de9e9cbfc1f1de3480
Rain Holloway <rainh2001@gmail.com>
2021-10-17 22:01:23 +1100

    Load level 1 is now functional

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public enum GameState { Start, Level1, Level2 }
+    public static GameState currentGameState = GameState.Start;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
