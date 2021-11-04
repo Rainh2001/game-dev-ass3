d3cc7cdb007615f730b9516edf6c576731cba5e2
Rain Holloway <rainh2001@gmail.com>
2021-11-04 13:25:14 +1100

    Innovation scene added and linked to start screen with level 2 button

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -28,14 +28,22 @@ public class SceneLoader : MonoBehaviour
     public void loadLevel1(){
         SceneManager.LoadSceneAsync(1);
         SceneManager.sceneLoaded += OnSceneLoaded;
+        GameManager.currentGameState = GameManager.GameState.Level1;
+    }
+
+    public void loadLevel2(){
+        SceneManager.LoadSceneAsync(2);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        GameManager.currentGameState = GameManager.GameState.Level2;
     }
 
     public static void loadStartScreen(){
         SceneManager.LoadSceneAsync(0);
+        GameManager.currentGameState = GameManager.GameState.Start;
     }
 
     public void OnSceneLoaded(Scene scene, LoadSceneMode mode){
-        if(scene.buildIndex == 1){
+        if(scene.buildIndex == 1 || scene.buildIndex == 2){
             button = GameObject.FindWithTag("QuitButton").GetComponent<Button>();
             button.onClick.AddListener(loadStartScreen);
         } else if(scene.buildIndex == 0){
